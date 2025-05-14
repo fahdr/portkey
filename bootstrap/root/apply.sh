9f1d552f96872424dfaf0d7fb7966c4fde631828
fahd <fahdr@outlook.com>
2025-05-14 20:40:12 +0000

    Remove Gitea references and related configurations from various files

--- bootstrap/root/apply.sh
+++ bootstrap/root/apply.sh
@@ -3,9 +3,6 @@
 bash -c "source .cephrc && $(wget -qLO - https://raw.githubusercontent.com/rook/rook/release-1.10/deploy/examples/import-external-cluster.sh)"
 VALUES="values.yaml"
 
-kubectl get ingress gitea --namespace gitea \
-    || VALUES="values-seed.yaml"
-
 helm template \
     --include-crds \
     --namespace argocd \
